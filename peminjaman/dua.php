27cb6a119b6de52a9bba7904543e812c5a9e6acb
dediarpandi <dediarpandi@gmail.com>
2016-12-04 11:24:35 +0700

    Commit coba 1
    coba pertama

---
+++ peminjaman/dua.php
@@ -0,0 +1,12 @@
+<?php
+
+$x = 15;
+
+//if ( syarat) { maka }
+if( $x == 15 ){
+	echo "Angka Benar";
+}else {
+	echo "Angka Salah";
+}
+
+?>
---
+++ peminjaman/satu.php
@@ -0,0 +1,19 @@
+<?php
+
+//tanda dolar digunakan ntuk tipe data
+$nama = "Dedi Arpandi";
+$umur = 21;
+
+echo "<h1> HALAMAN UTAMA </h1>";
+echo "Belajar PHP <br><br><br>";
+echo "$nama <br> $umur <br>";
+
+$x = 10;
+$y = 5;
+
+echo $x + $y;
+
+
+
+
+?>
